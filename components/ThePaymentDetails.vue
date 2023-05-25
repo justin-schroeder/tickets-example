41bdeccf6f5d401e3b570a1a56ce44252015790e
Justin Schroeder <justin@formkit.com>
2023-05-25 16:31:55 -0500

    The intial starting point

---
+++ components/ThePaymentDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Payment details</h2>
+  <FormKit
+    type="mask"
+    label="Credit Card"
+    mask="####-####-####-####"
+    name="cc_number"
+    prefix-icon="visa"
+    validation="required"
+    overlay
+  />
+  <FormKit
+    type="datepicker"
+    name="cc_expiration"
+    format="MMMM YYYY"
+    label="Expiration date"
+    overlay
+    validation="required|date_after"
+  />
+  <FormKit type="mask" name="cc_cvv" mask="###" label="Security code" overlay />
+  <ThePrice
+    :prices="value.tickets?.map(sumPrice) || []"
+    prefix="Final price: "
+  />
+</template>
---
+++ utils/workshops.ts
@@ -0,0 +1,27 @@
+export const workshops = [
+  { value: { workshop: "none", price: 0 }, label: "No workshop" },
+  {
+    value: { workshop: "reactivity", price: 600 },
+    label: "Reactivity w/ Evan You",
+  },
+  {
+    value: { workshop: "intro", price: 600 },
+    label: "Getting started w/ Adam Jahr",
+  },
+  {
+    value: { workshop: "patterns", price: 600 },
+    label: "Proven patterns w/ Ben Hong",
+  },
+  {
+    value: { workshop: "testing", price: 600 },
+    label: "Testing w/ Jessica Sachs",
+  },
+  {
+    value: { workshop: "nuxt", price: 600 },
+    label: "Nuxt w/ Daniel Roe",
+  },
+  {
+    value: { workshop: "pinia", price: 600 },
+    label: "Pinia w/ Alex Kyriakidis",
+  },
+];
